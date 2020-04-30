bbb52e463dbec227ac62919fb459b27f03f93401
DanilNazarov <62388738+DanilNazarov@users.noreply.github.com>
2020-04-30 17:25:04 +0300

    Add files via upload

---
+++ PRAC2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PRAC2
+{
+    partial class Nazarov
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtOtv = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y =";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(87, 124);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(104, 20);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(87, 180);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(104, 20);
+            this.txtY.TabIndex = 3;
+            // 
+            // txtOtv
+            // 
+            this.txtOtv.Location = new System.Drawing.Point(36, 266);
+            this.txtOtv.Multiline = true;
+            this.txtOtv.Name = "txtOtv";
+            this.txtOtv.Size = new System.Drawing.Size(346, 172);
+            this.txtOtv.TabIndex = 4;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(267, 143);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(114, 37);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Пуск";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Nazarov
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtOtv);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Nazarov";
+            this.Text = "Nazarov";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtOtv;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ PRAC2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRAC2
+{
+    public partial class Nazarov : Form
+    {
+        // <summary>
+        /// Класс Nazarov производит инициализацию компонентов
+        /// </summary>
+        public Nazarov()
+        {
+            InitializeComponent();
+            txtX.Text = "" + 1.825 * Math.Pow(10, 2);
+            txtY.Text = "18,225";
+        }
+        ///<summary>
+        /// Метод Reshenie(),который выполняет решение примера 
+        ///</summary>
+        ///<param name="х">1 число</param>
+        ///<param name="у">2 число</param>
+        private void Reshenie(ref double x, ref double y)
+        {
+            double c = 0;
+            double db = Math.Pow(x, 2);
+            double db1 = Math.Pow(y, 3);
+            double db2 = y * (1 - x);
+            double db3 = Math.Abs(x) + y;
+            double otvet = db + db1 - (db2 / db3);
+            txtOtv.Text = c + "= " + otvet.ToString();
+        }
+        ///<summary>
+        ///Метод btnStart_Click,которыйзапускаетметод Reshenie() 
+        ///</summary>
+        ///<param name="sender">Объект,который вызывает событие</param>
+        ///<param name="e">Дает возможность передать значения обработчику</param>
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(txtX.Text);
+            double y = double.Parse(txtY.Text);
+            Reshenie(ref x, ref y);
+
+        }
+    }
+
+}
